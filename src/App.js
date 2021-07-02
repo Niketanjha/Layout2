0e93b744934327cf365d1feeac6b5ab0cbd253f4
Niketan <niketanjha@gmail.com>
2021-07-02 11:41:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state={cardInfo:[
-      {title:'Feature Label', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-      {title:'Feature Label', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-      {title:'Feature Label', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-      {title:'Feature Label', content:'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
+      {id:'1', item:''},
+      {id:'2', item:''},
+      {id:'3', item:''},
+      {id:'4', item:''},
      ]};
   }
-  
+
   render(){
     return(
       <div className="mainClass">
@@ -26,44 +26,37 @@ class App extends React.Component{
 
         <div>
       <div className="marginAll">
-        <div className="text">
-              <h1 className="sample" style={{fontFamily:'sans-serif',fontWeight:'200'}}>Sample heading</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elt. lpsa, ipsam, eligendi, in quo sunt possimus non
-                incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat. 
-              </p>
-              <button>Call to action!</button>
-            </div>
-          </div>
-          <div className="line2">
-            <div className="heading2">
-              <p  >Latest Features</p>
-            </div>
-          </div>
-
+        <div className="firstText">
+          <h1 className="sample" style={{fontFamily:'sans-serif',fontWeight:'200'}}>Sample heading</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elt. lpsa, ipsam, eligendi, in quo sunt possimus non
+            incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat. 
+          </p>
+          <button>Call to action!</button>
+        </div>
+      </div>
+      <div className="line">
+        <div className="heading"><p>Latest Features</p></div>
+      </div>
           {this.state.cardInfo.map((card, index)=>{
             return(
-                <div className="row2">
-                <div className="column2">
-                  <div className="card4">
-                  <div className="fakeimg2" style={{fontSize:'larger'}}>
-                    <span style>800X500</span>
-                  </div>
-                  <h3 style={{fontSize:"large",fontFamily:'sans-serif'}}>Feature Label</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  </p>
-                  <button className="btn1">Buy now</button>
-                  <button className="btn2">More info</button>
+                <div className="row">
+                  <div className="column">
+                    <div className="cards">
+                      <div className="imageClass" style={{fontSize:'larger'}}>
+                        <span >800X500</span>
+                      </div>
+                      <h3 style={{fontSize:"large",fontFamily:'sans-serif'}}>Feature Label</h3>
+                      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                      <button className="buyNow">Buy now</button>
+                      <button className="moreInfo">More info</button>
+                    </div>
                   </div>
                 </div>
-          </div>
             )
         })}
           <hr style={{width:'88%',color:'#c0c0c0',marginLeft:'6%',marginTop:"30%"}}></hr>
           <p className="copyright"> Copyright @your Website 2014</p>
         </div>
-
       </div>
     );
   }
